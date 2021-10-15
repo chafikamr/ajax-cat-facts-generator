deeb943de5d1ffb85481a2c8d4369e015d5f2f3d
Chafik Amraoui <chafikamraoui0@gmail.com>
2021-10-15 01:06:20 +0100

    fix body overflow when loading animation is active

--- js/main.js
+++ js/main.js
@@ -9,18 +9,28 @@ $(function() {
             url: 'https://catfact.ninja/fact',
 
             beforeSend: function() {
+                $('body').css('overflow', 'hidden');
+                $('body').css('height', '100vh');
                 document.getElementById('loading').style.display = "flex"
             },
             success: function(data, two, three) {
                 let dt = JSON.stringify(data.fact)
                     // $('#img').attr('src', data.message)
+
                 $('.fact').text(dt)
+
             },
             error: function() {
 
             },
             complete: function(data) {
-                setTimeout(function() { document.getElementById('loading').style.display = "none" }, 500)
+                setTimeout(function() {
+                    document.getElementById('loading').style.display = "none"
+
+                    $('body').css('overflow', 'auto');
+                    $('body').css('height', 'unset');
+
+                }, 500)
 
             }
         })
